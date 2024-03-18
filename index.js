f8a15f2659a51ab961d3b4d15026d3784bd08733
Pras2214 <parikhprasann@gmail.com>
2024-03-18 10:43:17 +0530

    Added the total variable to the db

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ const formDataSchema = new mongoose.Schema({
   combo1: Number,
   combo2: Number,
   paymentMode: String,
+  total: Number
 });
 
 const FormData = mongoose.model("FormData", formDataSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -51,22 +51,11 @@ app.get("/", (req, res) => {
 
 app.post("/submit-form", async (req, res) => {
   res.redirect("/");
-  const formData = new FormData({
-    name: req.body.name,
-    pNumber: "+91" + req.body.pNumber,
-    skippi: req.body.skippiCount || 0,
-    tosteta: req.body.tosteta,
-    tostetaCount: req.body.tostetaCount || 0,
-    coffee: req.body.coffeeCount || 0,
-    combo1: req.body.combo1Count || 0,
-    combo2: req.body.combo2Count || 0,
-    paymentMode: req.body["payment-mode"],
-  });
-  const receipientName = req.body.name;
   const receipientNumber = "91" + req.body.pNumber;
   const skippi = parseInt(req.body.skippiCount) || 0;
   const toasteta = "toasteta" + (req.body.tosteta || 3);
   const toastetaCount = parseInt(req.body.tostetaCount) || 0;
+  const receipientName = req.body.name;
   const coffee = parseInt(req.body.coffeeCount) || 0;
   const combo1 = parseInt(req.body.combo1Count) || 0;
   const combo2 = parseInt(req.body.combo2Count) || 0;
@@ -79,7 +68,19 @@ app.post("/submit-form", async (req, res) => {
     combo1 * pricing["combo1"] +
     combo2 * pricing["combo2"];
 
-  finalTotal = total;
+  const formData = new FormData({
+    name: req.body.name,
+    pNumber: "+91" + req.body.pNumber,
+    skippi: req.body.skippiCount || 0,
+    tosteta: req.body.tosteta,
+    tostetaCount: req.body.tostetaCount || 0,
+    coffee: req.body.coffeeCount || 0,
+    combo1: req.body.combo1Count || 0,
+    combo2: req.body.combo2Count || 0,
+    paymentMode: req.body["payment-mode"],
+    total : total
+  });
+
 
   const message = `Hello ${receipientName}, Thank you for ordering at Tangy Tales. You paid a total of ${total}₹ in ${paymentMode} mode. Do visit us again!! 😊`;
 
